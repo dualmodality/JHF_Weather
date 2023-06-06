b89b61d22e9d8da4d7058918a636422a8c4e6648
dualmodality <dualmodality@gmail.com>
2023-06-05 21:25:34 -0500

    All Widgets/Text Added and placed: now for formatting...

--- app/src/main/java/com/example/jhf_weather/MainActivity.kt
+++ app/src/main/java/com/example/jhf_weather/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.jhf_weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jhf_weather.ui.theme.JHF_WeatherTheme
 
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CurrentWeather()
                 }
             }
         }
@@ -30,17 +36,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CurrentWeather() {
+    Column {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier
+
+        )
+        Text(
+            text = stringResource(id = R.string.my_location),
+            modifier = Modifier
+        )
+        Row {
+            Column {
+                Text(
+                    text = stringResource(id = R.string.current_temp_data),
+                    modifier = Modifier
+                )
+                Row {
+                    Text(
+                        text = stringResource(id = R.string.feels_like),
+                        modifier = Modifier
+                    )
+                    Spacer(modifier = Modifier)
+                    Text(
+                        text = stringResource(id = R.string.feels_like_data),
+                        modifier = Modifier
+                    )
+                }
+            }
+            Image(painter = painterResource(id = R.drawable.sunny),
+                    contentDescription = stringResource(id = R.string.img_description)
+            )
+        }
+        Row {
+            Text(text = stringResource(id = R.string.daily_low))
+            Spacer(modifier = Modifier)
+            Text(text = stringResource(id = R.string.daily_low_data))
+        }
+        Row {
+            Text(text = stringResource(id = R.string.daily_high))
+            Spacer(modifier = Modifier)
+            Text(text = stringResource(id = R.string.daily_high_data))
+        }
+        Row {
+            Text(text = stringResource(id = R.string.humidity))
+            Spacer(modifier = Modifier)
+            Text(text = stringResource(id = R.string.humidity_data))
+        }
+        Row {
+            Text(text = stringResource(id = R.string.atmos_pressure))
+            Spacer(modifier = Modifier)
+            Text(text = stringResource(id = R.string.atmos_data))
+
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CurrentWeatherPreview() {
     JHF_WeatherTheme {
-        Greeting("Android")
+        CurrentWeather()
     }
 }
