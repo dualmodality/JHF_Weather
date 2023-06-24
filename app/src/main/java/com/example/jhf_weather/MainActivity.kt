c7cb1e6e91a058d5ccfcac11abe8716634ede216
dualmodality <dualmodality@gmail.com>
2023-06-24 16:10:24 -0400

    Implemented Title Bar as TopAppBar Object

--- app/src/main/java/com/example/jhf_weather/MainActivity.kt
+++ app/src/main/java/com/example/jhf_weather/MainActivity.kt
@@ -14,10 +14,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,6 +34,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jhf_weather.ui.theme.JHF_WeatherTheme
 import com.example.jhf_weather.ui.theme.PurpleGrey40
+import com.example.jhf_weather.ui.theme.PurpleGrey80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,18 +53,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CurrentWeather() {
     Column(modifier = Modifier.fillMaxSize()) {
-        Text(
-            text = stringResource(id = R.string.app_name),
-            style = MaterialTheme.typography.titleSmall,
-            color = Color(red = 200, green = 200, blue = 200),
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(PurpleGrey40)
-                .padding(8.dp)
+        TopAppBar(
+            title = { Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.titleSmall,
+                color = Color(red = 200, green = 200, blue = 200),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
 
+            ) },
+           colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = PurpleGrey40)
         )
         Text(
             text = stringResource(id = R.string.my_location),
@@ -74,24 +84,33 @@ fun CurrentWeather() {
                     style = MaterialTheme.typography.bodyLarge,
                     modifier = Modifier.fillMaxWidth()
                 )
-                Row(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()) {
                     Text(
                         text = stringResource(id = R.string.feels_like),
                         style = MaterialTheme.typography.bodySmall,
-                        modifier = Modifier.weight(2f).padding(4.dp)
+                        modifier = Modifier
+                            .weight(2f)
+                            .padding(4.dp)
                     )
                     Spacer(modifier = Modifier)
                     Text(
                         text = stringResource(id = R.string.feels_like_data),
                         style = MaterialTheme.typography.bodySmall,
-                        modifier = Modifier.weight(1f).padding(4.dp)
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(4.dp)
                     )
                 }
             }
             Spacer(modifier = Modifier)
             Image(painter = painterResource(id = R.drawable.sunny),
                 contentDescription = stringResource(id = R.string.img_description),
-                modifier = Modifier.weight(0.4f).clip(shape = CircleShape).size(160.dp)
+                modifier = Modifier
+                    .weight(0.4f)
+                    .clip(shape = CircleShape)
+                    .size(160.dp)
             )
         }
         Row {
