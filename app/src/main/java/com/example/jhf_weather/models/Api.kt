82b2e1da56f9dbac22eefa56fea0068e68d7560d
dualmodality <dualmodality@gmail.com>
2023-07-16 21:11:54 -0500

    July 16 commit 2

--- app/src/main/java/com/example/jhf_weather/models/Api.kt
+++ app/src/main/java/com/example/jhf_weather/models/Api.kt
@@ -1,5 +1,11 @@
 package com.example.jhf_weather.models
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,4 +17,16 @@ interface Api {
         @Query(value = "units") units: String = "imperial",
         @Query(value = "appid") appid: String = "2ba6a68c2752676b1f6a031bb637be59"
     ) : CurrentConditions
+}
+
+@Module
+@InstallIn(ActivityComponent::class)
+object Api {
+    @Provides
+    fun provideCurrentConditions(): CurrentConditions {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build()
+            .create(CurrentConditions::class.java)
+    }
 }
